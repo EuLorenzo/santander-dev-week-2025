c4e4f2cc3d4dc0aa9722a755adedae36db4fefd9
euLorenzo <lorenzo.frinhani@gmail.com>
2025-08-22 11:16:05 -0300

    Criacao camada de servico + criacao de um GlobalExceptionHandler + adicionado swagger ao projeto

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        Long userId = userToCreate.getId();
+
+        if(userId != null && userRepository.existsById(userId)) {
+            throw new IllegalArgumentException("User with id " + userId + " already exists");
+        }
+
+        return userRepository.save(userToCreate);
+    }
+}
